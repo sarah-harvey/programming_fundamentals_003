7840fa719b37eb29c50ddc8af8cc84aa039683df
Sarah Harvey <s_harvey15@yahoo.com>
2019-10-08 10:25:34 +0100

    work in progress, paused to amend 001/002

--- test/catalogue_service.test.js
+++ test/catalogue_service.test.js
@@ -1,6 +1,9 @@
 const catalogueService = require("../app/catalogue_service");
 
 describe("catalogueService", () => {
+
+
+
   describe("catalogueService.checkBook", () => {
     test("returns true when a book is found by title", () => {
       expect(catalogueService.checkBook("Great Expectations")).toBe(true);
@@ -18,4 +21,31 @@ describe("catalogueService", () => {
       expect(catalogueService.checkBook("The Wind")).toBe(false);
     });
   });
+
+  ///////
+
+  describe("catalogueService.countBooksByKeyword", () => {
+    test("returns true when a book is found by keyword", () => {
+      expect(catalogueService.countBooksByKeyword("Great Expectations")).toBe(true);
+    });
+
+    test("returns false when a book cannot be found by keyword", () => ) {
+      expect(catalogueService.countBooksByKeyword("Gone With The Wind")).toBe(false);
+    };
+
+    test("returns true when a partial match is found", () => {
+      expect(catalogueService.countBooksByKeyword("Expectations")).toBe(true);
+    });
+  });
+
+  describe("catalogueService.getBooksByAuthor", () => {
+    test("it returns an array of Charles Dickens books", () => {
+      const actual = catalogueService.getBooksByAuthor("Charles Dickens");
+      const expected = ["A Tale of Two Cities", "Oliver Twist", "Great Expectations"];
+      expect(actual).toEqual(expected)
+    });
+  });
+
+
+
 });
